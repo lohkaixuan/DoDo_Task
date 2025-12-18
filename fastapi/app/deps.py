6887530cd016eeb08a91280879bc9ae26650e0af
Qi70619 <132583062+Qi70619@users.noreply.github.com>
2025-12-18 11:47:29 +0800

    edit deps.py

--- fastapi/app/deps.py
+++ fastapi/app/deps.py
@@ -1,23 +1,24 @@
 # app/deps.py
-
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
-from app.models.user import User
+from bson import ObjectId
+
 from app.config import settings
+from app.models.user import User
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
     try:
         payload = jwt.decode(token, settings.JWT_SECRET, algorithms=[settings.JWT_ALG])
-        email = payload.get("sub")  # ✅ sub = email
-        if not email:
+        uid = payload.get("sub")  # ✅ sub = user_id (ObjectId string)
+        if not uid:
             raise HTTPException(status_code=401, detail="Invalid token payload")
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid/expired token")
 
-    user = await User.find_one(User.email == email)
+    user = await User.find_one(User.id == ObjectId(uid))
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
